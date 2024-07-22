10dda4edd2763b1e299fe2b0485609275b4cfe0a
mazay33 <mazaev2000@gmail.com>
2024-07-22 16:48:20 +0300

    fix: delete count from wishlist

---
+++ prisma/migrations/20240718071410_delete_count_from_wishlist_product/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `count` on the `wishlist_products` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "wishlist_products" DROP COLUMN "count";
